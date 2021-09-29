5412714fd596e04f5d03f0a8c346fcb4f501cecd
Nailson97 <nailsonsousa97@gmail.com>
2021-09-29 20:46:00 -0300

    projeto terminado

--- filmes/src/Routes.jsx
+++ filmes/src/Routes.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Erro from './pages/Erro/index'
+import Filme from './pages/Filme/index'
 
 const Routes = () => {
     return(
@@ -10,7 +11,8 @@ const Routes = () => {
             <Header/>
             <Switch>
                 <Route exact path='/' component={Home}/>
-                <Route patch='#' component={Erro}/>
+                <Route exact path='/filme/:id' component={Filme}/>
+                <Route path='*' component={Erro}/>
             </Switch>
         </BrowserRouter>
     )
